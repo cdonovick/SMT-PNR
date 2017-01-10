7b02f9a637963e47b57e738ca6478c9858dffe5a
makaimann <makaim@stanford.edu>
2017-01-10 10:24:38 -0800

    Created a new hamming function which uses z3.LShR instead of >> z3 interprets >> as an arithmetic shift which can cause the hamming distance to be miscalculated when the MSB = 1

--- z3util.py
+++ z3util.py
@@ -51,7 +51,32 @@ def hamming_d(bv):
 
     return x & (2**b_point - 1)
 
-hamming = hamming_d
+#replaced arithmetic shift with logical shift
+def hamming_e(bv):
+    '''
+    Based on popcount64b from https://en.wikipedia.org/wiki/Hamming_weight 
+    '''
+    bsize = bv.size()
+    b_point = bsize.bit_length()
+
+    s = 2**((bsize - 1).bit_length())
+
+    max_exp = (s - 1).bit_length()
+    mvals = [(2**i, build_grouped_mask(2**i, bsize).value) for i in range(max_exp)]
+    x = bv - (z3.LShR(bv, mvals[0][0]) & mvals[0][1])
+    x = (x & mvals[1][1]) + (z3.LShR(x, mvals[1][0]) & mvals[1][1])
+    
+    for idx,i in enumerate(mvals[2:]):
+        x = (x + z3.LShR(x, i[0])) & i[1]
+        if i[0] > b_point:
+            break
+
+    for i in mvals[idx+3:]:
+        x += z3.LShR(x, i[0])
+
+    return x & (2**b_point - 1)    
+
+hamming = hamming_e
 
 #used in testing
 _GIANT_NUMBER = 5016456510113118655434598811035278955030765345404790744303017523831112055108147451509157692220295382716162651878526895249385292291816524375083746691371804094271873160484737966720260389217684476157468082573 * 14197795064947621068722070641403218320880622795441933960878474914617582723252296732303717722150864096521202355549365628174669108571814760471015076148029755969804077320157692458563003215304957150157403644460363550505412711285966361610267868082893823963790439336411086884584107735010676915
