4a1e29416a60d09048084892201f585041a1f2a2
makaimann <makaim@stanford.edu>
2017-03-17 15:02:22 -0700

    Added monosat model reader

--- src/pnr/model_readers.py
+++ src/pnr/model_readers.py
@@ -7,3 +7,24 @@ def place_model_reader(position_type):
 
     return reader
 
+
+def route_model_reader(fabric, design, p_state, r_state, vars, solver):
+    model = solver.get_model()
+    for src in design.modules:
+        for port, net in src.outputs.items():
+            src_pos = p_state[src]
+            dst_pos = p_state[net.dst]
+            src_pe = fab[src_pos].PE
+            dst_pe = fab[dst_pos].PE
+            reaches = solver.g.reaches(src_pe.getPort('out'), dst_pe.getPort(port))
+            l = model.getPath(reaches)
+            for n1, n2 in zip(l, l[1:]):
+                edge = model.getEdge(l[0], l[1])
+                track = vars.I[edge]
+                src_port = track.src
+                dst_port = track.dst
+                outname = track.names[1]
+                inname = track.names[0]
+                state = (src_port.x, src_port.y, track.element, outname, inname)
+                r_state[net] = state
+    
