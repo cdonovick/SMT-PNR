1cc38b5da92c14f83d89eb97b91b0cf88aeacb56
makaimann <makaim@stanford.edu>
2017-03-17 15:16:11 -0700

    Updated imports
    fabric and fabricfuns import eachother (not using from imports)

--- src/fabric/fabricfuns.py
+++ src/fabric/fabricfuns.py
@@ -1,3 +1,4 @@
+import fabric
 '''
    Functions to be used in fabric.py
 '''
@@ -7,15 +8,15 @@ def getSide(side_str):
        Takes a string and returns the corresponding side
     '''
     if side_str == 'N':
-        return Side.N
+        return fabric.Side.N
     elif side_str == 'S':
-        return Side.S
+        return fabric.Side.S
     elif side_str == 'E':
-        return Side.E
+        return fabrci.Side.E
     elif side_str == 'W':
-        return Side.W
+        return fabric.Side.W
     elif side_str == 'NS':
-        return Side.NS
+        return fabric.Side.NS
     else:
         raise ValueError('Not passed a valid side string')
 
@@ -23,16 +24,16 @@ def mapSide(x, y, side):
     '''
        Given a location and a side, returns the receiving tile location and receiving side
     '''
-    if side is Side.N:
-        return x, y+1, Side.S
-    elif side is Side.S:
-        return x, y-1, Side.N
-    elif side is Side.E:
-        return x+1, y, Side.W
-    elif side is Side.W:
-        return x-1, y, Side.E
+    if side is fabric.Side.N:
+        return x, y+1, fabric.Side.S
+    elif side is fabric.Side.S:
+        return x, y-1, fabric.Side.N
+    elif side is fabric.Side.E:
+        return x+1, y, fabric.Side.W
+    elif side is fabric.Side.W:
+        return x-1, y, fabric.Side.E
     else:
-        raise ValueError('Expected a Side but got {}'.format(type(side)))
+        raise ValueError('Expected a fabric.Side but got {}'.format(type(side)))
 
 
 def parse_name(text):
@@ -40,4 +41,4 @@ def parse_name(text):
         Takes a (non-PE) port name and returns direction, BUS width, side, track number
     '''
     s = text.split('_')
-    return s[0], s[1], Side(int(s[2][1])), int(s[3][1])
+    return s[0], s[1], fabric.Side(int(s[2][1])), int(s[3][1])
