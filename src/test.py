367cc83b172e4ac91dde22330f7ea4d837d1badf
makaimann <makaim@stanford.edu>
2017-03-22 18:06:24 -0700

    Added IO_OUTPUT_FILE to tester

--- src/test.py
+++ src/test.py
@@ -2,14 +2,15 @@
 import sys
 import design, design.core2graph, fabric, pnr, smt
 
-if len(sys.argv) != 4:
-    print('usage: {} <DESIGN_FILE> <FABRIC_FILE> <OUTPUT_FILE>'.format(sys.argv[0]))
+if len(sys.argv) != 5:
+    print('usage: {} <DESIGN_FILE> <FABRIC_FILE> <OUTPUT_FILE> <IO_OUTPUT_FILE>'.format(sys.argv[0]))
     sys.exit(1)
 
 
 DESIGN_FILE = sys.argv[1] 
 FABRIC_FILE = sys.argv[2]
 OUTPUT_FILE = sys.argv[3]
+IO_OUTPUT_FILE = sys.argv[4]
 
 POSITION_T = smt.BVXY
 PLACE_CONSTRAINTS = pnr.init_positions(POSITION_T), pnr.distinct, pnr.nearest_neighbor, pnr.pin_IO
@@ -43,4 +44,5 @@ else:
     sys.exit(1)
 
 print("Writing design to {}".format(OUTPUT_FILE))
-p.write_design(pnr.write_to_xml(FABRIC_FILE, OUTPUT_FILE))
+print("Writing io settings to {}".format(IO_OUTPUT_FILE))
+p.write_design(pnr.write_to_xml(FABRIC_FILE, OUTPUT_FILE, IO_OUTPUT_FILE))
