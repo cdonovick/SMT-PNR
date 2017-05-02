1060014a76bcd6b8ff4e2bcded652deb0cbc0f15
Caleb Donovick <donovick@cs.stanford.edu>
2017-05-02 12:16:14 -0700

    Add bitsream writer

--- src/pnr/__init__.py
+++ src/pnr/__init__.py
@@ -1,5 +1,7 @@
 from .constraints import *
 from .model_readers import *
 from .model_writer import *
+from .bitstream_writer import bitstream_writer
 from .design_printer import *
 from .pnr import *
+
