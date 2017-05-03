3d5c09ca471d16d8f03adbc4bf2e5cc7804e1b38
makaimann <makaim@stanford.edu>
2017-05-03 13:11:07 -0700

    Letting script modify environment variables

--- get_solvers.sh
+++ get_solvers.sh
@@ -4,13 +4,13 @@ solver_dirs=(solver_binaries)
 
 for solver_dir in "${solver_dirs[@]}"
 do
-    if [ -d "`pwd`$solver_dir" ]; then
+    if [ -d "`pwd`/$solver_dir" ]; then
         echo "Found solvers"
     else
         echo "Retrieving solvers"
         wget http://web.stanford.edu/~makaim/files/solver_binaries.tar.gz
         tar -xzvf ./solver_binaries.tar.gz
     fi
-    export PYTHONPATH=$PYTHONPATH:$PWD/solver_binaries
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/solver_binaries
+    export PYTHONPATH=$PYTHONPATH:`pwd`/solver_binaries
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/solver_binaries
 done
