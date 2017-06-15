210aad411920550643c76e4252bfcf44016a483f
makaimann <makaim@stanford.edu>
2017-06-15 12:26:10 -0700

    Hack for input wires

--- src/design/design.py
+++ src/design/design.py
@@ -13,6 +13,54 @@ class Design(NamedIDObject):
         _mods = SortedDict()
         _nets = dict()
 
+        # HACK to make only one wire leave input
+        # to help CGRA team with io hack
+        # creating a dummy PE (i.e. input + 0)
+        hackidx = 0
+        newmodules = {}
+        input2adders = {}
+        input2consts = {}
+        for mod_name, args in modules.items():
+            if args['type'] == 'IO' and args['conf'] == 'i':
+                addname = 'iohackadder{}'.format(hackidx)
+                constname = 'iohackconst{}'.format(hackidx)
+                newadderargs = {'res': Resource.PE, 'type': 'PE', 'conf': 'add'}
+                newadder = {addname: newadderargs}
+                newconstargs = {'res': Resource.UNSET, 'type': 'Const', 'conf': 0}
+                newconst = {constname: newconstargs}
+                newmodules.update(newadder)
+                newmodules.update(newconst)
+                input2adders[mod_name] = addname
+                input2consts[mod_name] = constname
+                hackidx += 1
+
+        # add the new hack modules to modules
+        modules.update(newmodules)
+
+        newnets = set()
+        toremove = set()
+        # replace input with dummy adder
+        for src_name, src_port, dst_name, dst_port, width in nets:
+            if src_name in input2adders:
+                toremove.add((src_name, src_port, dst_name, dst_port, width))
+                newnets.add((input2adders[src_name], src_port, dst_name, dst_port, width))
+
+        # remove the input elements
+        nets = nets - toremove
+
+        # replace with dummy adder
+        nets = nets.union(newnets)
+
+        # make const 0 --> dummyadder connection
+        for io, const in input2consts.items():
+            nets.add((const, 'out', input2adders[io], 'b', 16))
+
+        # connect the input to the dummy adder
+        for io, adder in input2adders.items():
+            nets.add((io, 'pe_out_res', adder, 'a', 16))
+
+        # end HACK for IOs
+
 
         #build modules
         for mod_name,args in modules.items():
