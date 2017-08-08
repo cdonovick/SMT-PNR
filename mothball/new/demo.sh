1c8e2773e08ca00cba6a652bee5bacbc824b1e04
Caleb Donovick <donovick@cs.stanford.edu>
2017-08-08 12:20:31 -0700

    Delete mothball

--- mothball/new/demo.sh
+++
@@ -1,5 +0,0 @@
-#run demo file
-python demo.py ../../test/examples/downCast_13_reduce.dot
-
-#convert .dot file to .png
-#dot -Kneato -n -Tpng -o display.png display.dot
--- mothball/new/test.py
+++
@@ -1,4 +0,0 @@
-#import global_router as gb
-#gb.test('../examples/NMS_10_reduce.dot',(8,8))
-import tests
-tests.tiny_opt_test()
