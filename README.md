000ef4272fcc5f6fed33e53b539f3223575822d6
Caleb Donovick <donovick@cs.stanford.edu>
2017-08-09 14:28:46 -0700

    Add pip support

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 #SMT based tool for place and route
 [![Build Status](https://travis-ci.org/cdonovick/SMT-PNR.svg?branch=master)](https://travis-ci.org/cdonovick/SMT-PNR)
 
-currently a code sausage; dont look at how it is made
-
-
+#Install
+pip3 install -e .
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+        name='pnrdoctor',
+        version='0.1',
+        description='SMT based place and route tool',
+        author='Caleb Donovick, Makai Mann',
+        author_email='donovick@cs.stanford.edu',
+        license='MIT',
+        packages=['pnrdoctor'],
+        install_requires=['coreir', 'monosat>=1.4',], #should inculde smt-switch
+        python_requires='>=3.4'
+)
+
