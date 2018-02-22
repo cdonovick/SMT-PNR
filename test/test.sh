4596c4d7410b7695ed95a0e00133fcfb13310489
makaimann <makaim@stanford.edu>
2018-02-22 11:36:05 -0800

    Point test.sh at new fabric

--- test/test.sh
+++ test/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PREFIX=$(dirname $0)
 TESTS=$PREFIX/designs/*
-FABRICS=$PREFIX/cgra_info.txt
+FABRICS=$PREFIX/fabrics/*
 LIBS="cgralib"
 ANNOTATED="${PREFIX}/annotated"
 COMMANDS="--time --print --annotate $ANNOTATED"
