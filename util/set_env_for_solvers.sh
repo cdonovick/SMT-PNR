c64c24ee81fe910f66d627efe982a315a7c741a9
Lenny Truong <lenny@stanford.edu>
2018-05-30 09:57:15 -0700

    Add support for specifying the solver path via environment variable

--- util/set_env_for_solvers.sh
+++ util/set_env_for_solvers.sh
@@ -1,14 +1,16 @@
 echo "Adding paths to solvers"
 
-export PYTHONPATH=$PYTHONPATH:`pwd`/smt_solvers/cvc4
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/smt_solvers/cvc4
+: "${SMT_SOLVER_PATH:=$PWD}"
 
-export PATH=$PATH:`pwd`/smt_solvers/monosat
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/smt_solvers/monosat
+export PYTHONPATH=$PYTHONPATH:$SMT_SOLVER_PATH/smt_solvers/cvc4
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SMT_SOLVER_PATH/smt_solvers/cvc4
 
-export PATH=$PATH:`pwd`/smt_solvers/z3/bin/
-export PYTHONPATH=$PYTHONPATH:`pwd`/smt_solvers/z3/bin/python/
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/smt_solvers/z3/bin
+export PATH=$PATH:$SMT_SOLVER_PATH/smt_solvers/monosat
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SMT_SOLVER_PATH/smt_solvers/monosat
 
-export PYTHONPATH=$PYTHONPATH:`pwd`/smt_solvers/boolector
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/smt_solvers/boolector
+export PATH=$PATH:$SMT_SOLVER_PATH/smt_solvers/z3/bin/
+export PYTHONPATH=$PYTHONPATH:$SMT_SOLVER_PATH/smt_solvers/z3/bin/python/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SMT_SOLVER_PATH/smt_solvers/z3/bin
+
+export PYTHONPATH=$PYTHONPATH:$SMT_SOLVER_PATH/smt_solvers/boolector
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SMT_SOLVER_PATH/smt_solvers/boolector
